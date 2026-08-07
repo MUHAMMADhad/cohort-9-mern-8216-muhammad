92e26359b86c5fcb881b6ef4bfffd625ec08c010
Muhammad Hadi <muhammadhadiofficial123@gmail.com>
2026-08-07 20:16:07 +0500

    Initialize backend project setup

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import pinoHttp from "pino-http"
+
+import logger from "./config/logger.js";
+
+const app = express();
+const API_PREFIX = "/api/v1";
+
+// Security Middleware
+app.use(helmet());
+
+// Enable CORS
+app.use(cors());
+
+// Parse JSON Request Body
+app.use(express.json());
+
+app.use(
+    pinoHttp({
+        logger,
+    })
+);
+
+// Health Check Route (Testing)
+app.get(`${API_PREFIX}/health`, (req, res)=>{
+    res.status(200).json({
+        success: true,
+        message: "Notes API is running",
+    });
+});
+// For testing that an api is running or not! 
+
+export default app;
