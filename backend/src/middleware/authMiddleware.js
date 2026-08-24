a1c913fb4472892b6260fff6757d0505036bdd06
Muhammad Hadi <muhammadhadiofficial123@gmail.com>
2026-08-24 15:51:25 +0500

    Updated frontend!

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -22,7 +22,7 @@ const authMiddleware = (req, res, next) => {
     req.user = decoded;
 
     next();
-  } catch (error) {
+  } catch {
     return res.status(401).json({
       success: false,
       message: "Invalid or expired token",
